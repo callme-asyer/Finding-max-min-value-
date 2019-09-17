bb26691350029073770845854c7c052a7098f84c
callme-asyer <53029091+callme-asyer@users.noreply.github.com>
2019-09-17 08:47:51 +0700

    Tugas niomic
    mencari nilai terbesar dan terkecil

---
+++ tugas4.js
@@ -0,0 +1,50 @@
+function perbandingan(){
+  var tinggi_abdul=220;
+  var tinggi_satria=200; //tertinggi sekarang
+  var tinggi_andi=250;
+
+ if(tinggi_abdul>tinggi_satria && tinggi_abdul>tinggi_andi){
+  if(tinggi_andi>tinggi_satria && tinggi_andi<tinggi_abdul){
+    console.log("Siswa tertinggi yaitu abdul dengan tinggi " + tinggi_abdul);
+    console.log("siswa tertinggi kedua yaitu andi dengan tinggi " + tinggi_andi);
+    console.log("jadi siswa yang paling rendah adalah satria dengan tinggi " + tinggi_satria);
+
+
+  }  if(tinggi_abdul>tinggi_satria && tinggi_abdul>tinggi_andi){
+      if(tinggi_satria>tinggi_andi && tinggi_satria<tinggi_abdul)
+      console.log("Siswa tertinggi yaitu abdul dengan tinggi " + tinggi_abdul);
+      console.log("siswa tertinggi kedua yaitu satria dengan tinggi " + tinggi_satria);
+      console.log("jadi siswa yang paling rendah adalah andi dengan tinggi " + tinggi_andi);
+    }
+
+}  if(tinggi_satria>tinggi_abdul && tinggi_satria>tinggi_andi){
+    if(tinggi_abdul>tinggi_andi && tinggi_abdul<tinggi_satria)
+    console.log("Siswa tertinggi yaitu satria dengan tinggi " + tinggi_satria);
+    console.log("siswa tertinggi kedua yaitu abdul dengan tinggi " + tinggi_abdul);
+    console.log("jadi siswa yang paling rendah adalah andi dengan tinggi " + tinggi_andi);
+
+
+
+      }  if(tinggi_satria>tinggi_abdul && tinggi_satria>tinggi_andi){
+            if(tinggi_andi>tinggi_abdul &&tinggi_andi<tinggi_satria){
+              console.log("Siswa tertinggi yaitu satria dengan tinggi " + tinggi_satria);
+              console.log("siswa tertinggi kedua yaitu andi dengan tinggi " + tinggi_andi);
+              console.log("jadi siswa yang paling rendah adalah abdul dengan tinggi " + tinggi_abdul);
+      }
+    }if(tinggi_andi>tinggi_satria && tinggi_andi>tinggi_abdul){
+        if(tinggi_satria>tinggi_abdul && tinggi_satria<tinggi_andi){
+          console.log("Siswa tertinggi yaitu andi dengan tinggi " + tinggi_andi);
+          console.log("siswa tertinggi kedua yaitu satria dengan tinggi " + tinggi_satria);
+          console.log("jadi siswa yang paling rendah adalah abdul dengan tinggi " + tinggi_abdul);
+
+  }
+  if(tinggi_andi>tinggi_satria && tinggi_andi>tinggi_abdul){
+      if(tinggi_abdul>tinggi_satria && tinggi_abdul<tinggi_andi){
+        console.log("Siswa tertinggi yaitu andi dengan tinggi " + tinggi_andi);
+        console.log("siswa tertinggi kedua yaitu abdul dengan tinggi " + tinggi_abdul);
+        console.log("jadi siswa yang paling rendah adalah satria dengan tinggi " + tinggi_satria);
+}
+ }
+}
+}
+perbandingan();
